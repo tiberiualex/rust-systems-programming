5116280820141232455bfdb90431e434052bee97
tiberiualex <tiberiu.alexander@gmail.com>
2021-05-03 22:51:20 +0300

    Add chapter2 projet

---
+++ chapter2/target/rls/debug/deps/chapter2-145644f3162f9c69.d
@@ -0,0 +1,5 @@
+/home/tiberiu/Desktop/Work/rust/rust-systems-programming/chapter2/target/rls/debug/deps/chapter2-145644f3162f9c69.rmeta: src/main.rs
+
+/home/tiberiu/Desktop/Work/rust/rust-systems-programming/chapter2/target/rls/debug/deps/chapter2-145644f3162f9c69.d: src/main.rs
+
+src/main.rs:
---
+++ chapter2/target/rls/debug/deps/chapter2-ff310aa435b7987b.d
@@ -0,0 +1,5 @@
+/home/tiberiu/Desktop/Work/rust/rust-systems-programming/chapter2/target/rls/debug/deps/chapter2-ff310aa435b7987b.rmeta: src/main.rs
+
+/home/tiberiu/Desktop/Work/rust/rust-systems-programming/chapter2/target/rls/debug/deps/chapter2-ff310aa435b7987b.d: src/main.rs
+
+src/main.rs:
